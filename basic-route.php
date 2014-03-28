f3261fdfadc1a8aa699841896e4fd1f1773f9cb3
paper-paulon <paper.paulon@gmail.com>
2014-03-28 13:06:55 +0800

    Create basic-route.php

---
+++ basic-route.php
@@ -0,0 +1,23 @@
+//--> Basic route GET
+Route::get('/', function()
+{
+    return 'Hello World';
+});
+
+//--> Basic route POST
+Route::post('foo/bar', function()
+{
+    return 'Hello World';
+});
+
+//--> Registering A Route For Multiple Verbs
+Route::match(array('GET', 'POST'), '/', function()
+{
+    return 'Hello World';
+});
+
+//--> Forcing A Route To Be Served Over HTTPS
+Route::get('foo', array('https', function()
+{
+    return 'Must be over HTTPS';
+}));
